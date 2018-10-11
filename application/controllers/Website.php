ad133dacba6f42d037af6cf4cca5869080970436
ebrych <ebrych@gmail.com>
2018-10-11 12:32:00 -0500

    Update Website.php

--- application/controllers/Website.php
+++ application/controllers/Website.php
@@ -1,11 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 class Website extends CI_Controller{
-
+     
+     public $controlador='xx';
+     
      public function getData(){
+          $this->load->model('DataModel');
+          $data['datos']= $this->DataModel->infoWeb();
+          $this->load->view('index',$data);
      }
      public function getLocales(){
+          $this->load->model('DataModel');
+          $data['datos']= $this->DataModel->LocalSelectList();
+          $this->load->view('index',$data);
      }
      public function getContacto(){
+          
      }
 }
